6ce9d8b3712b5ca7efb736db6cfbe21c8d166119
Fernando <Pimenta>
2019-12-27 10:46:59 -0300

    sync s3

--- digisin_facade.py
+++ digisin_facade.py
@@ -1,5 +1,7 @@
+from bisect import bisect_left
+from pathlib import Path
 import boto3
-import json
+import os
 from time import sleep
 
 job1 = 'awsCliJob1'
@@ -16,6 +18,12 @@ sucesso = 'SUCCE'
 falha = 'FAIL'
 
 bucket = 'dev-brasilseg-ged'
+
+s3_to_local_map = [
+    {'key': '/12', 'pasta_local': '/home/nova/Documents/git/digisincontrolm/files/12'},
+    {'key': '/14', 'pasta_local': '/home/nova/Documents/git/digisincontrolm/files/14'}
+]
+pasta_destino = '/home/nova/Documents/git/digisincontrolm/files'
 
 boto3.setup_default_session(profile_name=profile_name)
 client = boto3.client('batch')
@@ -36,7 +44,7 @@ def is_pode_continuar(jobId):
             raise Exception(f'Falha no job: {jobId}')
 
 
-def start_aws_batch_sync(jobName, fila_job, def_job,):
+def start_aws_batch_sync(jobName, fila_job, def_job, ):
     print(f'Startando aws batch {def_job}')
     responseJob1 = client.submit_job(
         jobName=jobName,
@@ -51,5 +59,10 @@ def start_aws_batch_sync(jobName, fila_job, def_job,):
 if __name__ == '__main__':
     start_aws_batch_sync(job1, fila_job1, def_job1)
 
-    #  todo implementar papel do datasync
+    for map in s3_to_local_map:
+        print(map)
+        sync = f'aws s3 sync s3://{bucket}{map["key"]} {map["pasta_local"]} --profile {profile_name}'
+        print(sync)
+        os.system(sync)
+
     start_aws_batch_sync(job2, fila_job2, def_job2)
